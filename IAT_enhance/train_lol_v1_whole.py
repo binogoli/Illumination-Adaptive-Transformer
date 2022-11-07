e6cd0f6cfe8b9b81946b9e79f400885534370353
li <gcc_bingo@163.com>
2022-11-07 14:08:42 +0800

    1

--- IAT_enhance/train_lol_v1_whole.py
+++ IAT_enhance/train_lol_v1_whole.py
@@ -19,101 +19,105 @@ from data_loaders.lol_v1_whole import lowlight_loader_new
 from model.IAT_main import IAT
 
 from IQA_pytorch import SSIM
-from utils import PSNR, adjust_learning_rate, validation, LossNetwork, visualization, CharbonnierLoss
+from utils import PSNR, adjust_learning_rate, validation, LossNetwork, visualization
+#    , CharbonnierLoss
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--gpu_id', type=str, default=0)
-parser.add_argument('--img_path', type=str, default='/data/unagi0/cui_data/light_dataset/LOL_v1/our485/low/')
-parser.add_argument('--img_val_path', type=str, default='/data/unagi0/cui_data/light_dataset/LOL_v1/eval15/low/')
 
-parser.add_argument('--batch_size', type=int, default=8)
-parser.add_argument('--lr', type=float, default=1e-4)
-parser.add_argument('--weight_decay', type=float, default=0.0001)
-parser.add_argument('--pretrain_dir', type=str, default='workdirs/snapshots_folder_lol_v1_patch/best_Epoch.pth')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--gpu_id', type=str, default=0)
+    parser.add_argument('--img_path', type=str, default='./data/our485/low/')
+    parser.add_argument('--img_val_path', type=str, default='./data/eval15/low/')
 
-parser.add_argument('--num_epochs', type=int, default=400)
-parser.add_argument('--display_iter', type=int, default=10)
-parser.add_argument('--snapshots_folder', type=str, default="workdirs/snapshots_folder_lol_v1_whole")
+    parser.add_argument('--batch_size', type=int, default=1)
+    parser.add_argument('--lr', type=float, default=1e-4)
+    parser.add_argument('--weight_decay', type=float, default=0.0001)
+    parser.add_argument('--pretrain_dir', type=str, default=None)
 
-config = parser.parse_args()
+    parser.add_argument('--num_epochs', type=int, default=400)
+    parser.add_argument('--display_iter', type=int, default=10)
+    parser.add_argument('--snapshots_folder', type=str, default="workdirs/snapshots_folder_lol_v1_whole")
 
-print(config)
-os.environ['CUDA_VISIBLE_DEVICES'] = str(config.gpu_id)
+    config = parser.parse_args()
 
-if not os.path.exists(config.snapshots_folder):
-    os.makedirs(config.snapshots_folder)
+    print(config)
+    os.environ['CUDA_VISIBLE_DEVICES'] = str(config.gpu_id)
 
-# Model Setting
-model = IAT().cuda()
-if config.pretrain_dir is not None:
-    model.load_state_dict(torch.load(config.pretrain_dir))
+    if not os.path.exists(config.snapshots_folder):
+        os.makedirs(config.snapshots_folder)
 
-# Data Setting
-train_dataset = lowlight_loader_new(images_path=config.img_path)
-train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=config.batch_size, shuffle=True, num_workers=8,
-                                           pin_memory=True)
-val_dataset = lowlight_loader_new(images_path=config.img_val_path, mode='test')
-val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=1, shuffle=False, num_workers=8, pin_memory=True)
+    # Model Setting
+    model = IAT().cuda()
+    if config.pretrain_dir is not None:
+        model.load_state_dict(torch.load(config.pretrain_dir))
 
-optimizer = torch.optim.Adam(model.parameters(), lr=config.lr, betas=(0.9, 0.999), eps=1e-8, weight_decay=config.weight_decay)
-scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=config.num_epochs)
+    # Data Setting
+    train_dataset = lowlight_loader_new(images_path=config.img_path)
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=config.batch_size, shuffle=True, num_workers=1,
+                                               pin_memory=True)
+    val_dataset = lowlight_loader_new(images_path=config.img_val_path, mode='test')
+    val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=1, shuffle=False, num_workers=8, pin_memory=True)
 
-device = next(model.parameters()).device
-print('the device is:', device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=config.lr, betas=(0.9, 0.999), eps=1e-8,
+                                 weight_decay=config.weight_decay)
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=config.num_epochs)
 
-# Loss & Optimizer Setting & Metric
-vgg_model = vgg16(pretrained=True).features[:16]
-vgg_model = vgg_model.cuda()
+    device = next(model.parameters()).device
+    print('the device is:', device)
 
-for param in vgg_model.parameters():
-    param.requires_grad = False
+    # Loss & Optimizer Setting & Metric
+    vgg_model = vgg16(pretrained=True).features[:16]
+    vgg_model = vgg_model.cuda()
 
-#L1_loss = CharbonnierLoss()
-L1_loss = nn.L1Loss()
-L1_smooth_loss = F.smooth_l1_loss
+    for param in vgg_model.parameters():
+        param.requires_grad = False
 
-loss_network = LossNetwork(vgg_model)
-loss_network.eval()
+    # L1_loss = CharbonnierLoss()
+    L1_loss = nn.L1Loss()
+    L1_smooth_loss = F.smooth_l1_loss
 
-ssim = SSIM()
-psnr = PSNR()
-ssim_high = 0
-psnr_high = 0
+    loss_network = LossNetwork(vgg_model)
+    loss_network.eval()
 
-model.train()
-print('######## Start IAT Training #########')
-for epoch in range(config.num_epochs):
-    # adjust_learning_rate(optimizer, epoch)
-    print('the epoch is:', epoch)
-    for iteration, imgs in enumerate(train_loader):
-        low_img, high_img = imgs[0].cuda(), imgs[1].cuda()
-        # Checking!
-        #visualization(low_img, 'show/low', iteration)
-        #visualization(high_img, 'show/high', iteration)
-        optimizer.zero_grad()
-        model.train()
-        mul, add, enhance_img = model(low_img)
+    ssim = SSIM()
+    psnr = PSNR()
+    ssim_high = 0
+    psnr_high = 0
 
-        loss = L1_loss(enhance_img, high_img)
-        #loss = L1_smooth_loss(enhance_img, high_img)+0.04*loss_network(enhance_img, high_img)
-        loss.backward()
-        
-        optimizer.step()
-        scheduler.step()
+    model.train()
+    print('######## Start IAT Training #########')
+    for epoch in range(config.num_epochs):
+        # adjust_learning_rate(optimizer, epoch)
+        print('the epoch is:', epoch)
+        for iteration, imgs in enumerate(train_loader):
+            low_img, high_img = imgs[0].cuda(), imgs[1].cuda()
+            # Checking!
+            # visualization(low_img, 'show/low', iteration)
+            # visualization(high_img, 'show/high', iteration)
+            optimizer.zero_grad()
+            model.train()
+            mul, add, enhance_img = model(low_img)
 
-        if ((iteration + 1) % config.display_iter) == 0:
-            print("Loss at iteration", iteration + 1, ":", loss.item())
+            loss = L1_loss(enhance_img, high_img)
+            # loss = L1_smooth_loss(enhance_img, high_img)+0.04*loss_network(enhance_img, high_img)
+            loss.backward()
 
-    # Evaluation Model
-    model.eval()
-    PSNR_mean, SSIM_mean = validation(model, val_loader)
+            optimizer.step()
+            scheduler.step()
 
-    with open(config.snapshots_folder + '/log.txt', 'a+') as f:
-        f.write('epoch' + str(epoch) + ':' + 'the SSIM is' + str(SSIM_mean) + 'the PSNR is' + str(PSNR_mean) + '\n')
+            if ((iteration + 1) % config.display_iter) == 0:
+                print("Loss at iteration", iteration + 1, ":", loss.item())
 
-    if SSIM_mean > ssim_high:
-        ssim_high = SSIM_mean
-        print('the highest SSIM value is:', str(ssim_high))
-        torch.save(model.state_dict(), os.path.join(config.snapshots_folder, "best_Epoch" + '.pth'))
+        # Evaluation Model
+        model.eval()
+        PSNR_mean, SSIM_mean = validation(model, val_loader)
 
-    f.close()
+        with open(config.snapshots_folder + '/log.txt', 'a+') as f:
+            f.write('epoch' + str(epoch) + ':' + 'the SSIM is' + str(SSIM_mean) + 'the PSNR is' + str(PSNR_mean) + '\n')
+
+        if SSIM_mean > ssim_high:
+            ssim_high = SSIM_mean
+            print('the highest SSIM value is:', str(ssim_high))
+            torch.save(model.state_dict(), os.path.join(config.snapshots_folder, "best_Epoch" + '.pth'))
+
+        f.close()
